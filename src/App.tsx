633f2e6852611bc4de961953d21c48282c9c86df
Arinji2 <arinjaydhar205@gmail.com>
2025-01-23 10:38:54 +0530

    Add Router Support + Vercel Support

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+
 function App() {
-  return <div className="w-full h-[100svh] "></div>;
+  return (
+    <div className="w-full h-fit flex flex-col items-center justify-start max-w-[1280px]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
